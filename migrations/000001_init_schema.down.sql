d1979a7e4091e19c6b8feb8253e3dc759bffcbd2
ferdi <ferdigithub@gmail.com>
2025-05-26 18:32:19 +0700

    add migrations

--- migrations/000001_init_schema.down.sql
+++ migrations/000001_init_schema.down.sql
@@ -1,1 +1,5 @@
-DROP TABLE IF EXISTS hello_table;
+DROP TABLE IF EXISTS loan;
+
+DROP TABLE IF EXISTS loan_investment;
+
+DROP TABLE IF EXISTS loan_status_history;
--- migrations/000001_init_schema.up.sql
+++ migrations/000001_init_schema.up.sql
@@ -1,4 +1,32 @@
-CREATE TABLE hello_table (
-    id bigint GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-    message text
+CREATE TYPE loan_status AS ENUM (
+'proposed','approved','invested','disbursed'
+)
+
+CREATE TABLE loan (
+    loan_id uuid PRIMARY KEY,
+    principal_amount bigint NOT NULL,
+    interest_rate bigint NOT NULL,
+    agreement_letter text,
+    status loan_status NOT NULL,
+    created_at timestamp with time zone NOT NULL,
+    updated_at timestamp with time zone,
+    disburse_at timestamp with time zone,
+    borrower_id uuid NOT NULL
+);
+
+CREATE TABLE loan_investment (
+    loan_investment_id uuid PRIMARY KEY,
+    loan_id uuid NOT NULL,
+    investor_id uuid NOT NULL,
+    amount bigint NOT NULL,
+    invested_at timestamp with time zone NOT NULL
+);
+
+CREATE TABLE loan_status_history (
+    loan_status_history_id BIGSERIAL PRIMARY KEY,
+    loan_id uuid NOT NULL,
+    before jsonb,
+    after jsonb,
+    updated_by uuid,
+    updated_at timestamp with time zone
 );
